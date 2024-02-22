5d2ce7a2c15181a0fbe18af91dfccf53a6f9bbc6
Aysenem ZOBALAK <yokaii.noodle@gmail.com>
2024-02-22 03:06:14 +0300

    X Button
    X Button

--- Assets/Scripts/MainSceneMenu.cs
+++ Assets/Scripts/MainSceneMenu.cs
@@ -6,14 +6,29 @@ using UnityEngine.SceneManagement;
 public class MainSceneMenu : MonoBehaviour
 {
     public GameObject mainPanel;
+    public GameObject xButton;
     void Start()
     {
+        Time.timeScale = 1f;
         mainPanel.SetActive(false);
     }
     public void LoadScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
     }
+    public void XButton()
+    {
+        Time.timeScale = 0f;
+        mainPanel.SetActive(true);
+        xButton.SetActive(false);
+    }
+    public void Continue()
+    {
+        Time.timeScale = 1f;
+        mainPanel.SetActive(false);
+        xButton.SetActive(true);
+    }
+
     public void QuitGame()
     {
         Application.Quit();
